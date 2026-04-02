307961cf1ce033dd5e37d281a125b8511f9a19b5
CrisMader <mader.projects@gmail.com>
2026-04-02 19:26:33 +0200

    Adding display grid to cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { snippets } from "./data/snippets"
 import { SnippetCard } from "./components/SnippetCard"
-import styles from '../styles/App.module.css'
+import styles from './styles/App.module.css'
 
 function App() {
   return (
-    <div className={styles.app}>
+    <>
 
       <h1>Snippets</h1>
 
-      {snippets.map(snippet => {
-        return <SnippetCard snippet={snippet} key={snippet.id} />
-      })}
-      
-    </div>
+      <div className={styles.app}>
+        {snippets.map(snippet => {
+          return <SnippetCard snippet={snippet} key={snippet.id} />
+        })}
+
+      </div>
+    </>
   )
 }
 
