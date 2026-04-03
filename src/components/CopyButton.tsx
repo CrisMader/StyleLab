8a9a528c4a54c1157015c5ac8d27d67e9e39c32a
CrisMader <mader.projects@gmail.com>
2026-04-03 12:12:55 +0200

    Button Copy created

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -1,9 +1,27 @@
-interface CopyText{
-    text: string
+import { useState } from "react"
+
+interface CopyButtonProps {
+    copyText: string
 }
 
-export const CopyButton = ({ copyText }: CopyText) =>{
+export const CopyButton = ({ copyText }: CopyButtonProps) => {
+
+    const [isCopied, setIsCopied] = useState(false)
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(copyText)
+        setIsCopied(true)
+
+        setTimeout(() => {
+            setIsCopied(false)
+        }, 2000)
+    }
+
     return (
-        <button></button>
+
+        <button onClick={handleCopy}>
+            {isCopied ? 'Copied!' : 'Copy CSS'}
+        </button>
+
     )
 }
