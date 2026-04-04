43eeb6e7cbfe6947c8993ec4b2006e08db9add60
CrisMader <mader.projects@gmail.com>
2026-04-04 21:52:51 +0200

    Added Category Filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import { snippets } from "./data/snippets"
 import { SnippetCard } from "./components/SnippetCard"
 import styles from './styles/App.module.css'
+import { useState } from "react"
+import { CategoryFilter } from "./components/CategoryFilter"
 
 function App() {
 
   const categories = [...new Set(snippets.map(snippet => snippet.category))]
+
+  const [selectedCategory, setSelectedCategory] = useState('All')
 
   return (
     <>
 
       <h1>Snippets</h1>
 
-      <div className={styles.app}>
-        {snippets.map(snippet => {
-          return <SnippetCard snippet={snippet} key={snippet.id} />
-        })}
+      <CategoryFilter categories={categories} onSelected={setSelectedCategory}/>
 
+      <div className={styles.app}>
+        {snippets
+          .filter(snippet => selectedCategory === 'All' || snippet.category === selectedCategory)
+          .map(snippet => (
+            <SnippetCard key={snippet.id} snippet={snippet} />
+          ))}
       </div>
     </>
   )
--- src/components/CategoryFilter.tsx
+++ src/components/CategoryFilter.tsx
@@ -1,9 +1,16 @@
-import { useState } from "react"
+interface CategoriesFilterProps {
+    categories: string[]
+    onSelected: (param: string) => void
+}
 
-export const CategoryFilter = ({ categories, filtered}) =>{
+export const CategoryFilter = ({ categories, onSelected}: CategoriesFilterProps) =>{
 
     return(
         <>
+        <button onClick={() => onSelected('All')}>All</button>
+        {categories.map(category => (
+            <button onClick={() => onSelected(category)} key={category}>{category}</button>
+        ))}
         </>
     )
 }
