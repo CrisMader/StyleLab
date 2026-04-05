f2bb22acbc4f5262b4e1b7b68816bcf2bfc47e18
CrisMader <mader.projects@gmail.com>
2026-04-05 20:47:22 +0200

    Creating BaseModel

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,2 +1,16 @@
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
+from .database import Base
+from datetime import datetime, timezone
 
+class Snippet(Base):
+    __tablename__ = "snippet"
 
+    id: Mapped[int]= mapped_column(primary_key=True)
+    title: Mapped[str]= mapped_column(String(120), nullable=False)
+    description: Mapped[str]= mapped_column(String(300), nullable=False)
+    category: Mapped[str]= mapped_column(String, nullable=False)
+    css_code: Mapped[str]= mapped_column(Text, nullable=False)
+    html_code: Mapped[str]= mapped_column(Text, nullable=False)
+    author: Mapped[str]= mapped_column(String, nullable=False)
+    created_at: Mapped[datetime]= mapped_column(default=lambda: datetime.now(timezone.utc))
