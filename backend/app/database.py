f800dfe40daef820daaf3eb0823c04f93761f7e2
CrisMader <mader.projects@gmail.com>
2026-04-05 21:49:33 +0200

    Created APIs

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,4 +1,5 @@
-from sqlalchemy import create_engine, sessionmaker, declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
 
---
+++ backend/app/main.py
@@ -0,0 +1,45 @@
+from .database import SessionLocal, Base, engine
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends, HTTPException
+from .models import Snippet
+from .schemas import SnippetCreate, SnippetResponse
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+@app.get("/snippets", response_model=list[SnippetResponse])
+def get_snippets(db: Session = Depends(get_db)):
+    snippets = db.query(Snippet).all()
+
+    if not snippets:
+        raise HTTPException(status_code=404, detail="Snippets not found")
+
+    return snippets
+
+@app.get("/snippets/{id}", response_model=SnippetResponse)
+def get_snippet(id: int, db: Session = Depends(get_db)):
+    snippet = db.query(Snippet).filter(Snippet.id == id).first()
+
+    if snippet is None:
+        raise HTTPException(status_code=404, detail="Snippet not found")
+
+    return snippet
+
+@app.post("/snippets", status_code=201, response_model=SnippetResponse)
+def create_snippet(snippet: SnippetCreate, db: Session = Depends(get_db)):
+    new_snippet = Snippet(**snippet.model_dump())
+    db.add(new_snippet)
+    db.commit()
+    db.refresh(new_snippet)
+
+    return new_snippet
+    
